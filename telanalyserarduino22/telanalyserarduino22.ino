38be06fa4d0152bb15e09681afb0d7348f0edb6e
DESKTOP-QR3J6T1\sidd1 <sidd19299@gmail.com>
2020-09-12 19:10:12 +0530

    Amit's 1st change -  DTMF func. enable and code correction, pin defination etc.

--- telanalyserarduino22/telanalyserarduino22.ino
+++ telanalyserarduino22/telanalyserarduino22.ino
@@ -1,44 +1,46 @@
 /* Telephone Analyser
     Version :- 0.1
-    Author :- Siddharth Singh
+    Author :- Siddharth Singh & Amit Raj
 */
 
 #include <LiquidCrystal.h>  //LCD intialize 
 const int rs = 14, en = 19 , d4 = 15, d5 = 16, d6 = 17, d7 = 18;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-#define linerelaypos 1
-#define linerelayneg 2
+//#define linerelaypos 6
+//#define linerelayneg 5
 #define powerpos 3
 #define powerneg 4
-#define ringrelaypos 5
-#define ringrelayneg 6
 #define dtmfrelaypos 7
 #define dtmfrelayneg 8
+
+/*#define ringrelaypos 5
+#define ringrelayneg 6
+
 #define tonerelaypos 9
 #define tonerelayneg 10
-#define transrelaypos
-#define transrelayneg
+*/
+#define teltestcalleridswitch 24
 
 
-#define manbut 11
-#define dtmfbutton 12
-#define ringbutton 13
-#define recbutton 14
-#define trabutton 15
-#define ansbutton 16
+//#define manbut 11
+//#define dtmfbutton 12
+//#define ringbutton 13
+//#define recbutton 14
+//#define trabutton 15
+//#define ansbutton 16
 
 
-#define teltestcalleridswitch
-#define inoutcallswitch
-#define repeatcallswitch
-#define manautoswitch
+//#define teltestcalleridswitch
+//#define inoutcallswitch
+//#define repeatcallswitch
+//#define manautoswitch
 
 
-#define d0  12//output of dtmf module to uC 
-#define d1  13// "
-#define d2  14// "
-#define d3  15// "
+//#define d0  30//output of dtmf module to uC 
+//#define d1  31// "
+//#define d2  32// "
+//#define d3  33// "
 
 //#define calleridrelay 9
 //#define calleridbutton 10
@@ -50,19 +52,20 @@ void setup() {
   // put your setup code here, to run once:
 
 
-  pinMode(linerelaypos, OUTPUT);
-  pinMode(linerelayneg, OUTPUT);
+//  pinMode(linerelaypos, OUTPUT);
+  //pinMode(linerelayneg, OUTPUT);
   pinMode(powerpos, OUTPUT);
   pinMode(powerneg, OUTPUT);
-  pinMode(ringrelaypos, OUTPUT);
-  pinMode(ringrelayneg, OUTPUT);
+  
+  //pinMode(ringrelayneg, OUTPUT);
   pinMode(dtmfrelaypos, OUTPUT);
   pinMode(dtmfrelayneg, OUTPUT);
-  pinMode(tonerelaypos, OUTPUT);
+  pinMode(teltestcalleridswitch, INPUT);
+  /*pinMode(tonerelaypos, OUTPUT);
+   pinMode(ringrelaypos, OUTPUT);
   pinMode(tonerelayneg, OUTPUT);
   pinMode(transrelaypos, OUTPUT);
   pinMode(transrelayneg, OUTPUT);
-
   pinMode(manbut, INPUT);
   pinMode(dtmfbutton, INPUT);
   pinMode(ringbutton, INPUT);
@@ -70,15 +73,15 @@ void setup() {
   pinMode(trabutton, INPUT);
   pinMode(ansbutton, INPUT);
 
-  pinMode(teltestcalleridswitch, INPUT);
+  
   pinMode(inoutcallswitch, INPUT);
   pinMode(repeatcallswitch, INPUT);
   pinMode(manautoswitch, INPUT);
-
-  pinMode(d0, INPUT);
-  pinMode(d1, INPUT);
-  pinMode(d2, INPUT);
-  pinMode(d3, INPUT);
+*/
+  pinMode(30, INPUT);
+  pinMode(31, INPUT);
+  pinMode(32, INPUT);
+  pinMode(33, INPUT);
 
 
 
@@ -86,7 +89,7 @@ void setup() {
   lcd.begin(16, 2);
   lcd.print("TCC-3200-S");
   delay(3000);
-
+  Serial.begin(9600);
 
 
 
@@ -94,97 +97,134 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+dialidbtest();
 }
 
 
-int dialidbtest
+int dialidbtest()
 {
   //for setting dial test or idp test
-  int a = 1;
-  if (a == 0)
-  {
-    //activate idp mode
-  }
-  if (a == 1)
-  {
+//  int a =  digitalRead(teltestcalleridswitch);
+//  if (a == 1)
+//  {
+//    //activate idp mode
+//  }
+//  if (a == 0)
+//  {
 
-    digitalWrite(ringrelaypos, 1);
+   /* digitalWrite(ringrelaypos, 1);
     digitalWrite(ringrelayneg, 1);
     digitalWrite(tonerelaypos, 1);
     digitalWrite(tonerelayneg, 1);
-
+*/
 
     //activate dial mode
     digitalWrite(dtmfrelaypos, 0);  //activate dtmf relay
     digitalWrite(dtmfrelayneg, 0);  //activate dtmf relay
     digitalWrite(powerneg, 0);  //activate power relay
     digitalWrite(powerpos, 0);  //activate power relay
+//Serial.println(d0);
+//Serial.println(d1);
+//Serial.println(d2);
+//Serial.println(d3);
 
+ int d0,d1,d2,d3,da,db;
+     d0 = digitalRead(30);
+     d1 = digitalRead(31);
+     d2 = digitalRead(32);
+     d3 = digitalRead(33);
     if ((d0 == 0) & (d1 == 0) & (d2 == 0) & (d3 == 1))
     {
-      lcd.print("1"); //lcd write
+      lcd.print("1");//lcd write
+      da = 1;
+      if(da != db){
+      Serial.println(da);
+      db = da;
+      }
     }
 
     if ((d0 == 0) & (d1 == 0) & (d2 == 1) & (d3 == 0))
     {
       lcd.print("2"); //lcd write
+      da = 2;
+      if(da != db){
+      Serial.println(da);
+      db = da;
+      }
     }
 
     if ((d0 == 0) & (d1 == 0) & (d2 == 1) & (d3 == 1))
     {
       lcd.print("3"); //lcd write
+     da = 3;
+      if(da != db){
+      Serial.println(da);
+      db = da;
+      }
     }
 
     if ((d0 == 0) & (d1 == 1) & (d2 == 0) & (d3 == 0))
     {
       lcd.print("4");  //lcd write
+      da = 4;
+      if(da != db){
+      Serial.println(da);
+      db = da;
+      }
     }
 
     if ((d0 == 0) & (d1 == 1) & (d2 == 0) & (d3 == 1))
     {
       lcd.print("5");  //lcd write
+      Serial.println(da);
     }
 
     if ((d0 == 0) & (d1 == 1) & (d2 == 1) & (d3 == 0))
     {
       lcd.print("6"); //lcd write
+      Serial.println(da);
     }
 
     if ((d0 == 0) & (d1 == 1) & (d2 == 1) & (d3 == 1))
     {
       lcd.print("7");  //lcd write
+      Serial.println("7");
     }
 
     if ((d0 == 1) & (d1 == 0) & (d2 == 0) & (d3 == 0))
     {
       lcd.print("8"); //lcd write
+      Serial.println("8");
     }
 
     if ((d0 == 1) & (d1 == 0) & (d2 == 0) & (d3 == 1))
     {
       lcd.print("9"); //lcd write
+      Serial.println("9");
     }
 
     if ((d0 == 1) & (d1 == 0) & (d2 == 1) & (d3 == 0))
     {
-      lcd.print("1"); //lcd write
+      lcd.print("10"); //lcd write
+      Serial.println("10");
     }
 
     if ((d0 == 1) & (d1 == 0) & (d2 == 1) & (d3 == 1))
     {
-      lcd.print("1"); //lcd write
+      lcd.print("*"); //lcd write
+      Serial.println("*");
     }
 
     if ((d0 == 1) & (d1 == 1) & (d2 == 0) & (d3 == 0))
     {
-      lcd.print("1"); //lcd write
+      lcd.print("#"); //lcd write
+      Serial.println("#");
     }
     //lcd display
-  }
+//  }
 
 }
-int ringbutton()
+/*int ringbutton()
 {
   //for activating ring fuunctions
   ringfunc() //run ringfunc
@@ -246,7 +286,7 @@ int ans()
 {
   //for activating answer function - for off hook position
 }
-
+/*
 
 /*
 
