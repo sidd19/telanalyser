ad5e3336d6dba0a3e5ef8217096021a8d272cece
Sidd <sidd19299@gmail.com>
2020-09-14 16:21:48 +0530

    dtmf and idp done

--- telanalyserarduino22/telanalyserarduino22.ino
+++ telanalyserarduino22/telanalyserarduino22.ino
@@ -6,21 +6,23 @@
 #include <LiquidCrystal.h>  //LCD intialize 
 const int rs = 14, en = 19 , d4 = 15, d5 = 16, d6 = 17, d7 = 18;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
+ unsigned long previousMillis = 0;
 //#define linerelaypos 6
 //#define linerelayneg 5
 #define powerpos 3
 #define powerneg 4
 #define dtmfrelaypos 7
 #define dtmfrelayneg 8
-
+#define pwdn 29
+#define std  28
+#define teltestcalleridswitch 24
 /*#define ringrelaypos 5
 #define ringrelayneg 6
 
 #define tonerelaypos 9
 #define tonerelayneg 10
 */
-#define teltestcalleridswitch 24
+
 
 
 //#define manbut 11
@@ -56,11 +58,16 @@ void setup() {
   //pinMode(linerelayneg, OUTPUT);
   pinMode(powerpos, OUTPUT);
   pinMode(powerneg, OUTPUT);
-  
-  //pinMode(ringrelayneg, OUTPUT);
   pinMode(dtmfrelaypos, OUTPUT);
   pinMode(dtmfrelayneg, OUTPUT);
   pinMode(teltestcalleridswitch, INPUT);
+  pinMode(pwdn, OUTPUT);
+  pinMode(std, INPUT);
+  pinMode(30, INPUT);
+  pinMode(31, INPUT);
+  pinMode(32, INPUT);
+  pinMode(33, INPUT);
+  //pinMode(ringrelayneg, OUTPUT);
   /*pinMode(tonerelaypos, OUTPUT);
    pinMode(ringrelaypos, OUTPUT);
   pinMode(tonerelayneg, OUTPUT);
@@ -78,18 +85,15 @@ void setup() {
   pinMode(repeatcallswitch, INPUT);
   pinMode(manautoswitch, INPUT);
 */
-  pinMode(30, INPUT);
-  pinMode(31, INPUT);
-  pinMode(32, INPUT);
-  pinMode(33, INPUT);
+  
 
 
 
-
+  Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.print("TCC-3200-S");
-  delay(3000);
-  Serial.begin(9600);
+  //delay(3000);
+  
 
 
 
@@ -97,141 +101,220 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 dialidbtest();
+idp();
 }
 
 
 int dialidbtest()
 {
-  //for setting dial test or idp test
-//  int a =  digitalRead(teltestcalleridswitch);
-//  if (a == 1)
-//  {
-//    //activate idp mode
-//  }
-//  if (a == 0)
-//  {
+
 
    /* digitalWrite(ringrelaypos, 1);
-    digitalWrite(ringrelayneg, 1);
-    digitalWrite(tonerelaypos, 1);
-    digitalWrite(tonerelayneg, 1);
-*/
+      digitalWrite(ringrelayneg, 1);
+      digitalWrite(tonerelaypos, 1);
+      digitalWrite(tonerelayneg, 1); 
+   */
+
 
     //activate dial mode
     digitalWrite(dtmfrelaypos, 0);  //activate dtmf relay
     digitalWrite(dtmfrelayneg, 0);  //activate dtmf relay
-    digitalWrite(powerneg, 0);  //activate power relay
-    digitalWrite(powerpos, 0);  //activate power relay
-//Serial.println(d0);
-//Serial.println(d1);
-//Serial.println(d2);
-//Serial.println(d3);
+    digitalWrite(powerneg, 0);      //activate power relay
+    digitalWrite(powerpos, 0);      //activate power relay
+    digitalWrite(pwdn, 0);          //deactivate power down pin
 
- int d0,d1,d2,d3,da,db;
-     d0 = digitalRead(30);
-     d1 = digitalRead(31);
-     d2 = digitalRead(32);
-     d3 = digitalRead(33);
-    if ((d0 == 0) & (d1 == 0) & (d2 == 0) & (d3 == 1))
-    {
+ int d0,d1,d2,d3,d4;
+     d0 = digitalRead(30);          //reading input pin Q1
+     d1 = digitalRead(31);          //reading input pin Q2
+     d2 = digitalRead(32);          //reading input pin Q3
+     d3 = digitalRead(33);          //reading input pin Q4
+     d4 = digitalRead(std);         //reading input pin "std"
+
+
+int idpinterval;
+ unsigned long idpd; 
+ int std1;
+ unsigned long currentMillis;
+ std1 = digitalRead(std);
+ currentMillis = millis();
+
+  if(std1 == 1)
+  {
+    //currentMillis = millis();
+     idpd = currentMillis - previousMillis;
+    Serial.println(idpd);
+    previousMillis = currentMillis;
+  }
+  if(std1 == 0)
+  {
+//    if(currentMillis >1280);
+//    {
+//      currentMillis = 0;
+//    }
+  }
+  
+  
+    if ((d0 == 0) & (d1 == 0) & (d2 == 0) & (d3 == 1)) // Decoding DTMF to Numeric "1"
+     {
+      
+        if(d4 == 1) //reading std input
+      {
       lcd.print("1");//lcd write
-      da = 1;
-      if(da != db){
-      Serial.println(da);
-      db = da;
+      Serial.println("1");
+      delay(200);
+      digitalWrite(pwdn, 1); //activate power down input
+      } 
+     }
+
+    if ((d0 == 0) & (d1 == 0) & (d2 == 1) & (d3 == 0))  // Decoding DTMF to Numeric "2" 
+    {
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("2");//lcd write
+      Serial.println("2");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
+      } 
+    }
+
+    if ((d0 == 0) & (d1 == 0) & (d2 == 1) & (d3 == 1))   // Decoding DTMF to Numeric "3"
+    {
+  
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("3");//lcd write
+      Serial.println("3");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
       }
     }
 
-    if ((d0 == 0) & (d1 == 0) & (d2 == 1) & (d3 == 0))
+    if ((d0 == 0) & (d1 == 1) & (d2 == 0) & (d3 == 0))   // Decoding DTMF to Numeric "4"
     {
-      lcd.print("2"); //lcd write
-      da = 2;
-      if(da != db){
-      Serial.println(da);
-      db = da;
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("4");//lcd write
+      Serial.println("4");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
       }
     }
 
-    if ((d0 == 0) & (d1 == 0) & (d2 == 1) & (d3 == 1))
+    if ((d0 == 0) & (d1 == 1) & (d2 == 0) & (d3 == 1))   // Decoding DTMF to Numeric "5"
     {
-      lcd.print("3"); //lcd write
-     da = 3;
-      if(da != db){
-      Serial.println(da);
-      db = da;
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("5");//lcd write
+      Serial.println("5");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
       }
     }
 
-    if ((d0 == 0) & (d1 == 1) & (d2 == 0) & (d3 == 0))
+    if ((d0 == 0) & (d1 == 1) & (d2 == 1) & (d3 == 0))   // Decoding DTMF to Numeric "6"
     {
-      lcd.print("4");  //lcd write
-      da = 4;
-      if(da != db){
-      Serial.println(da);
-      db = da;
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("6");//lcd write
+      Serial.println("6");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
       }
     }
 
-    if ((d0 == 0) & (d1 == 1) & (d2 == 0) & (d3 == 1))
+    if ((d0 == 0) & (d1 == 1) & (d2 == 1) & (d3 == 1))   // Decoding DTMF to Numeric "7"
     {
-      lcd.print("5");  //lcd write
-      Serial.println(da);
-    }
-
-    if ((d0 == 0) & (d1 == 1) & (d2 == 1) & (d3 == 0))
-    {
-      lcd.print("6"); //lcd write
-      Serial.println(da);
-    }
-
-    if ((d0 == 0) & (d1 == 1) & (d2 == 1) & (d3 == 1))
-    {
-      lcd.print("7");  //lcd write
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("7");//lcd write
       Serial.println("7");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
+      }
     }
 
-    if ((d0 == 1) & (d1 == 0) & (d2 == 0) & (d3 == 0))
+    if ((d0 == 1) & (d1 == 0) & (d2 == 0) & (d3 == 0))   // Decoding DTMF to Numeric "8"
     {
-      lcd.print("8"); //lcd write
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("8");//lcd write
       Serial.println("8");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
+      }
     }
 
-    if ((d0 == 1) & (d1 == 0) & (d2 == 0) & (d3 == 1))
+    if ((d0 == 1) & (d1 == 0) & (d2 == 0) & (d3 == 1))   // Decoding DTMF to Numeric "9"
     {
-      lcd.print("9"); //lcd write
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("9");//lcd write
       Serial.println("9");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
+      }
     }
 
-    if ((d0 == 1) & (d1 == 0) & (d2 == 1) & (d3 == 0))
+    if ((d0 == 1) & (d1 == 0) & (d2 == 1) & (d3 == 0))   // Decoding DTMF to Numeric "0"
     {
-      lcd.print("10"); //lcd write
-      Serial.println("10");
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("0");//lcd write
+      Serial.println("0");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
+      }
     }
 
-    if ((d0 == 1) & (d1 == 0) & (d2 == 1) & (d3 == 1))
+    if ((d0 == 1) & (d1 == 0) & (d2 == 1) & (d3 == 1))   // Decoding DTMF to Numeric "*"
     {
-      lcd.print("*"); //lcd write
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("*");//lcd write
       Serial.println("*");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
+      }
     }
 
-    if ((d0 == 1) & (d1 == 1) & (d2 == 0) & (d3 == 0))
+    if ((d0 == 1) & (d1 == 1) & (d2 == 0) & (d3 == 0))   // Decoding DTMF to Numeric "#"
     {
-      lcd.print("#"); //lcd write
+      if(d4 == 1) //reading std input
+      {
+      lcd.print("#");//lcd write
       Serial.println("#");
+      delay(200);
+      digitalWrite(pwdn, 1);  //activate power down input
+      }
     }
-    //lcd display
-//  }
+
 
 }
-/*int ringbutton()
+int idp()
 {
-  //for activating ring fuunctions
-  ringfunc() //run ringfunc
-  //LCD - ringing
-  delay(200);
+// int idpinterval;
+// unsigned long idpd; 
+// int std1;
+// unsigned long currentMillis;
+// std1 = digitalRead(std);
+//  if(std1 == 1)
+//  {
+//    currentMillis = millis();
+//  }
+//  if(std1 == 0)
+//  {
+//    idpinterval = currentMillis - previousMillis;
+//    Serial.println(idpinterval);
+//    previousMillis = currentMillis;
+//  }
+//  
+  
+  
 }
 
+/*
 int ringfunc()
 {
 
